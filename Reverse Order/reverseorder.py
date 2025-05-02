27767b745b5021f20e48c5cbccf80171e4ff0098
nupurkarim88 <nupurkarim88@gmail.com>
2025-05-02 08:11:05 +0600

    Reverse Order

---
+++ Reverse Order/reverseorder.py
@@ -0,0 +1,11 @@
+#START
+num = int(input("Enter a number: "))
+num = abs(num)
+count = 0
+while num > 0:
+    count += 1
+    num //= 10 
+if count == 0:
+    count = 1
+print("Total digits in the number:", count)
+#END
