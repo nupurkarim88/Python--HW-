8c306518562b6892867ef427f24db701bb52c82d
nupurkarim88 <nupurkarim88@gmail.com>
2025-05-23 17:13:43 +0600

    Congratulations message

---
+++ Congratulations message/prog1.py
@@ -0,0 +1,8 @@
+def string_operations():
+    name = input("Enter your name: ")
+
+    print("Your name in uppercase:", name.upper())
+    print("Your name in lowercase:", name.lower())
+    print("Number of letters in your name:", len(name))
+
+string_operations()
