ec5344275751fb9afd989d070d82b005f572b3d2
nupurkarim88 <nupurkarim88@gmail.com>
2025-06-02 13:32:44 +0600

    Months

--- Congratulations message/prog1.py
+++
@@ -1,8 +0,0 @@
-def string_operations():
-    name = input("Enter your name: ")
-
-    print("Your name in uppercase:", name.upper())
-    print("Your name in lowercase:", name.lower())
-    print("Number of letters in your name:", len(name))
-
-string_operations()
