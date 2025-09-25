888e9104f2eacd5ca76aadf317997905cb9ca564
nupurkarim88 <nupurkarim88@gmail.com>
2025-09-25 19:00:06 +0600

    Symetric value/hw.py

---
+++ Symetric value/hw.py
@@ -0,0 +1,11 @@
+#START
+Set1_A = {'blue', 'green'}
+Set2_A = {'blue', 'yellow'}
+symmetric_difference_A_operator = Set1_A ^ Set2_A
+print(f"Symmetric difference for Problem A (using ^): {symmetric_difference_A_operator}")
+
+Set1_B = {1, 2, 3, 4, 5}
+Set2_B = {1, 5, 6, 7, 8, 9}
+symmetric_difference_B_operator = Set1_B ^ Set2_B
+print(f"Symmetric difference for Problem B (using ^): {symmetric_difference_B_operator}")
+#END
